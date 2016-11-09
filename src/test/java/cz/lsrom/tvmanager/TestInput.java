6555f48165d6065fa7384f680b230154b648ede4
lsrom <lukas.srom@gmail.com>
2016-11-09 21:54:00 +0100

    Add class TestInput for holding test data
     Changes to be committed:
    	new file:   src/test/java/cz/lsrom/tvmanager/TestInput.java

---
+++ src/test/java/cz/lsrom/tvmanager/TestInput.java
@@ -0,0 +1,24 @@
+package cz.lsrom.tvmanager;
+
+import java.io.File;
+
+/**
+ * Created by lsrom on 11/9/16.
+ */
+public class TestInput {
+    public final File input;
+    public final String showName;
+    public final Integer season;
+    public final int episodeNum;
+    public final String resolution;
+    public final String directory;
+
+    public TestInput(File input, String showName, Integer season, int episodeNum, String resolution, String directory) {
+        this.input = input;
+        this.showName = showName;
+        this.season = season;
+        this.episodeNum = episodeNum;
+        this.resolution = resolution;
+        this.directory = directory;
+    }
+}
