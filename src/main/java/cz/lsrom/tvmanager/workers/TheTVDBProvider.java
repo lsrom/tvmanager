9e827c3b24d406c475a644556941abe3b3ac27e9
lsrom <lukas.srom@gmail.com>
2016-11-10 15:27:03 +0100

    Add method connect() which simplifies communication with the server
    Now all communication with TheTVDB server can go through this method and all public methods can simply call it with proper parameters.
    
    This makes the code more readable, shorter and easier to maintain.
    
     Changes to be committed:
    	modified:   src/main/java/cz/lsrom/tvmanager/workers/TheTVDBProvider.java

--- src/main/java/cz/lsrom/tvmanager/workers/TheTVDBProvider.java
+++ src/main/java/cz/lsrom/tvmanager/workers/TheTVDBProvider.java
@@ -1,19 +1,19 @@
 package cz.lsrom.tvmanager.workers;
 
 import com.google.gson.Gson;
+import com.sun.istack.internal.NotNull;
 
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
+import java.net.*;
 
 /**
  * Created by lsrom on 11/10/16.
  */
 public class TheTVDBProvider {
     private static final String THETVDB_API_KEY = "FA55DD7926C17088";
-    private static final String LOGIN_URL = "https://api.thetvdb.com/login";
+    private static final String BASE_URL = "https://api.thetvdb.com/";
+    private static final String LOGIN_URL = BASE_URL + "login";
+    private static final String SEARCH_FOR_SHOW_URL = BASE_URL + "search/series?name=";
 
     private JWTToken token;     // token acquired through authentication with TheTVDB API - must be included in all request
 
@@ -29,66 +29,90 @@ public class TheTVDBProvider {
      * TheTVDB API.
      *
      * In case of failure, when no token is acquired,
-     * @return
+     * @return New TheTVDBProvider object or null in case of failure.
      */
     public static TheTVDBProvider login (){
         Gson gson = new Gson();
-        TheTVDBLogin login = new TheTVDBLogin(THETVDB_API_KEY, "", "");     // create TVDB login object with API key
-
-        String authenticationJson = gson.toJson(login);                     // convert TVDB login object to JSON
-        HttpURLConnection http = null;
-
-        try {   // create HTTP connection with POST method for retrieving the JWT token
-            URL loginUrl = new URL(LOGIN_URL);
-            URLConnection connection = loginUrl.openConnection();
-            http = (HttpURLConnection) connection;
-
-            http.setRequestMethod("POST");
-            http.setDoOutput(true);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+        String tokenJson = null;
+        try {
+            tokenJson = connect(LOGIN_URL, null);   // try to acquire token json
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        if (http != null){
-            byte[] jsonBytes = authenticationJson.getBytes();
-            int jsonBytesLength = jsonBytes.length;
+        JWTToken token = gson.fromJson(tokenJson, JWTToken.class);  // parse JSON with token into JWTToken object
 
-            http.setFixedLengthStreamingMode(jsonBytesLength);      // set length of HTTP equest
+        if (token != null){     // if new token object is not null, return new instance of this class
+            return new TheTVDBProvider(token);
+        }
+
+        return null;            // if token wasn't successfully created return null
+    }
+
+    /**
+     * This method connects to TheTVDB servers and gets data from them. If the token parameter is null this method
+     * presumes that the token needs to be retrieved and uses POST method to get it as described on TheTVDB API page.
+     * In such casse the returned string is JSON with the JWT token.
+     *
+     * If token parameter is specified then this method presumes that data are about to be retrieved from the server and
+     * uses GET method to retrieve it. JSON wth this data is returned.
+     *
+     * If connection fails and/or no data are retrieved, null is returned.
+     *
+     * @param url URL which this method should access.
+     * @param token JWT authorization token - if null or empty, new one is retrieved.
+     * @return String with data received from the server or null if there was no such data.
+     * @throws IOException When connection to server fails.
+     */
+    private static String connect(@NotNull String url, String token) throws IOException {
+        Gson gson = new Gson();
+        byte[] outputBytes = null;      // will hold output data for POST method
+        int outputLength = 0;           // number of bytes of output data for POST method
+
+        HttpURLConnection http = null;
+
+        URL loginUrl = new URL(url);
+        URLConnection connection = loginUrl.openConnection();
+        http = (HttpURLConnection) connection;
+
+        if (token == null || token.isEmpty()){  // if token is not passed into this method then we must retrieve one
+            http.setRequestMethod("POST");      // retrieving the token requires POST method
+
+            TheTVDBLogin login = new TheTVDBLogin(THETVDB_API_KEY, "", "");     // create TheTVDB login object with API key
+            String authenticationJson = gson.toJson(login);                     // convert TheTVDB login object to JSON
+
+            outputBytes = authenticationJson.getBytes();        // get bytes of JSON authentication request
+            outputLength = outputBytes.length;
+
+            http.setFixedLengthStreamingMode(outputLength);      // set length of HTTP request
             http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-            try {
-                http.connect();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            http.setDoOutput(true);
+        } else {        // if token is passed then we are logged in and want to retrieve information from TheTVDB API
+            http.setRequestMethod("GET");   // communication with TheTVDB API requires GET method
+            http.setDoOutput(true);
+            http.setRequestProperty("Content-Type", "charset=UTF-8");
+            http.setRequestProperty("Authorization", "Bearer " + token);    // every call to the API must have authentication field set
+        }
 
+        http.connect();
+
+        if (outputBytes != null){       // if outputBytes is still null then there are no data to be send
             // write request for token to TheTVDB
             try (OutputStream os = http.getOutputStream()){
-                os.write(jsonBytes);
+                os.write(outputBytes);
             } catch (IOException e) {
                 e.printStackTrace();
-            }
-
-            JWTToken token = null;     // new token object for holding the token value
-
-            // read and parse response from TheTVDB with the JWT token
-            try (InputStream is = http.getInputStream()){
-                Gson tokenJSon = new Gson();
-
-                // create token object from input stream
-                token = tokenJSon.fromJson(getStringFromInputStream(is), JWTToken.class);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            // if all went well and the token is set, return new TVDBProvider object
-            if (token != null){
-                return new TheTVDBProvider(token);
             }
         }
 
-        // if something went wrong and we didn't acquire the JWT token, return null;
+        // read and parse response from TheTVDB - return string
+        try (InputStream is = http.getInputStream()){
+            return getStringFromInputStream(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // if something went wrong return null
         return null;
     }
 
