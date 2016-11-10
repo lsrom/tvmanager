fe9489d215a1ec8b4b6e624c3fbf4e6b69288fd3
lsrom <lukas.srom@gmail.com>
2016-11-10 15:13:29 +0100

    Fix javadoc in JWTToken
     Changes to be committed:
    	modified:   src/main/java/cz/lsrom/tvmanager/workers/JWTToken.java

--- src/main/java/cz/lsrom/tvmanager/workers/JWTToken.java
+++ src/main/java/cz/lsrom/tvmanager/workers/JWTToken.java
@@ -1,11 +1,9 @@
 package cz.lsrom.tvmanager.workers;
 
 /**
- * Created by lsrom on 11/10/16.
- */
-/**
  * Holds the JWT token from TheTVDB API. Token expires after 24 hours.
  * @see <url>https://api.thetvdb.com/swagger#/Authentication</url>
+ * Created by lsrom on 11/10/16.
  */
 public class JWTToken {
     private String token;   // string with token
@@ -24,5 +22,26 @@ public class JWTToken {
 
     public void setToken(String token) {
         this.token = token;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        JWTToken jwtToken = (JWTToken) o;
+
+        return token.equals(jwtToken.token);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return token.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return token;
     }
 }
