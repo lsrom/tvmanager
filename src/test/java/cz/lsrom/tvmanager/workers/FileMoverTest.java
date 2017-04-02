5c7cc17769e52d540d2452890d6c63714543223e
lsrom <lukas.srom@gmail.com>
2017-04-02 15:11:45 +0200

    Update FileMoverTest to use platform independent file separators

--- src/test/java/cz/lsrom/tvmanager/workers/FileMoverTest.java
+++ src/test/java/cz/lsrom/tvmanager/workers/FileMoverTest.java
@@ -1,6 +1,9 @@
 package cz.lsrom.tvmanager.workers;
 
 import org.junit.Test;
+
+import java.io.File;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,56 +13,56 @@ public class FileMoverTest {
 
     @Test
     public void testGetTargetPathNoFileSeparatorInShowDir (){
-        String expected = "/path/to/show/dir/Season 02/cool_show.mkv";
-        String path = FileMover.getTargetPath("/path/to/show/dir", "Season %2s", 2, "cool_show.mkv");
+        String expected = File.separator + "path" + File.separator + "to" + File.separator + "show" + File.separator + "dir" + File.separator + "Season 02" + File.separator + "cool_show.mkv";
+        String path = FileMover.getTargetPath(File.separator + "path" + File.separator + "to" + File.separator + "show" + File.separator + "dir", "Season %2s", 2, "cool_show.mkv");
 
         assertEquals(expected, path);
     }
 
     @Test
     public void testGetTargetPathFileSeparatorInShowDir (){
-        String expected = "/path/to/show/dir/Season 02/cool_show.mkv";
-        String path = FileMover.getTargetPath("/path/to/show/dir/", "Season %2s", 2, "cool_show.mkv");
+        String expected = File.separator + "path" + File.separator + "to" + File.separator + "show" + File.separator + "dir" + File.separator + "Season 02" + File.separator + "cool_show.mkv";
+        String path = FileMover.getTargetPath(File.separator + "path" + File.separator + "to" + File.separator + "show" + File.separator + "dir" + File.separator + "", "Season %2s", 2, "cool_show.mkv");
 
         assertEquals(expected, path);
     }
 
     @Test
     public void testGetTargetPathSeasonPaddingThree (){
-        String expected = "/path/to/show/dir/Season 002/cool_show.mkv";
-        String path = FileMover.getTargetPath("/path/to/show/dir", "Season %3s", 2, "cool_show.mkv");
+        String expected = File.separator + "path" + File.separator + "to" + File.separator + "show" + File.separator + "dir" + File.separator + "Season 002" + File.separator + "cool_show.mkv";
+        String path = FileMover.getTargetPath(File.separator + "path" + File.separator + "to" + File.separator + "show" + File.separator + "dir", "Season %3s", 2, "cool_show.mkv");
 
         assertEquals(expected, path);
     }
 
     @Test
     public void testGetTargetPathSeasonPaddingOne (){
-        String expected = "/path/to/show/dir/Season 2/cool_show.mkv";
-        String path = FileMover.getTargetPath("/path/to/show/dir", "Season %1s", 2, "cool_show.mkv");
+        String expected = File.separator + "path" + File.separator + "to" + File.separator + "show" + File.separator + "dir" + File.separator + "Season 2" + File.separator + "cool_show.mkv";
+        String path = FileMover.getTargetPath(File.separator + "path" + File.separator + "to" + File.separator + "show" + File.separator + "dir", "Season %1s", 2, "cool_show.mkv");
 
         assertEquals(expected, path);
     }
 
     @Test
     public void testGetTargetPathSeasonPaddingOneLongSeasonNumber (){
-        String expected = "/path/to/show/dir/Season 22/cool_show.mkv";
-        String path = FileMover.getTargetPath("/path/to/show/dir", "Season %1s", 22, "cool_show.mkv");
+        String expected = File.separator + "path" + File.separator + "to" + File.separator + "show" + File.separator + "dir" + File.separator + "Season 22" + File.separator + "cool_show.mkv";
+        String path = FileMover.getTargetPath(File.separator + "path" + File.separator + "to" + File.separator + "show" + File.separator + "dir", "Season %1s", 22, "cool_show.mkv");
 
         assertEquals(expected, path);
     }
 
     @Test
     public void testGetTargetPathEmptySeasonFormat (){
-        String expected = "/path/to/show/dir/cool_show.mkv";
-        String path = FileMover.getTargetPath("/path/to/show/dir", "", 2, "cool_show.mkv");
+        String expected = File.separator + "path" + File.separator + "to" + File.separator + "show" + File.separator + "dir" + File.separator + "cool_show.mkv";
+        String path = FileMover.getTargetPath(File.separator + "path" + File.separator + "to" + File.separator + "show" + File.separator + "dir", "", 2, "cool_show.mkv");
 
         assertEquals(expected, path);
     }
 
     @Test
     public void testGetTargetPathSeasonFormatWithoutFormatString (){
-        String expected = "/path/to/show/dir/Season/cool_show.mkv";
-        String path = FileMover.getTargetPath("/path/to/show/dir", "Season", 2, "cool_show.mkv");
+        String expected = File.separator + "path" + File.separator + "to" + File.separator + "show" + File.separator + "dir" + File.separator + "Season" + File.separator + "cool_show.mkv";
+        String path = FileMover.getTargetPath(File.separator + "path" + File.separator + "to" + File.separator + "show" + File.separator + "dir", "Season", 2, "cool_show.mkv");
 
         assertEquals(expected, path);
     }
